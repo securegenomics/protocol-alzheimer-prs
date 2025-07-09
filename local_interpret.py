22433e930247ede91f55dd2a108912c93a6e15cc
Barış Özmen <hbaristr@gmail.com>
2025-07-09 21:31:13 +0800

    print changes

--- local_interpret.py
+++ local_interpret.py
@@ -3,15 +3,23 @@ def local_interpret(prs: float) -> str:
     """
     Interpret the PRS score.
     """
-    print("Your polygenic risk score is:" + str(prs))
+    
+    print("\n\n" + "="*60)
+    print("Alzheimer's Disease Risk Assesment")
+    print("="*60 + "\n")
+    
+    print("Your polygenic risk score (PRS) is:" + str(prs))
     print()
     
+    
     if prs > 1.2:
-        print("Alzheimer's disease risk: HIGH RISK")
+        print("\033[91mHIGH RISK\033[0m")  # Red
     elif prs > 0.8:
-        print("Alzheimer's disease risk: MODERATE RISK")
+        print("\033[0mMODERATE RISK\033[0m")  # Normal
     else:
-        print("Alzheimer's disease risk: LOW RISK")
+        print("\033[92mLOW RISK\033[0m")  # Green
+        
+    print("\n" + "="*60 + "\n")
         
         
         
